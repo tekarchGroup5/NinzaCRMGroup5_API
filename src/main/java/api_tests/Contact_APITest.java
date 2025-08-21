c5e237b2dda7c10c33eba3c51b283a3103ad416e
Priyanka Shinde <priyankashinde@ramkrishnas-MacBook-Pro-2.local>
2025-08-21 17:58:40 -0500

    added contactAllFieldPayload.json,Contact_APITest.java and testng.xml

--- src/main/java/api_tests/Contact_APITest.java
+++ src/main/java/api_tests/Contact_APITest.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import api_POJOS.CreateContactClassic_POJO;
 import api_POJOS.CreateLeadClassic_POJO;
 import io.restassured.mapper.ObjectMapperType;
+import io.restassured.response.Response;
 
 public class Contact_APITest extends api_BaseTest {
 	
@@ -94,28 +95,29 @@ public class Contact_APITest extends api_BaseTest {
 	    test.get().pass("Contact with all fields created and validated successfully");
 	    
 	}
-	// Update Lead
+	// Update Contact
 	// -------------------------------
-	@Test(priority = 1,enabled=true)
+	@Test(priority = 1,enabled=false)
 	public void updateContact() throws IOException {
-	    test.set(extent.createTest("Update Lead Test"));
+	    test.set(extent.createTest("Update Contact Test"));
 
 	    ObjectMapper mapper = new ObjectMapper();
 
-	    // Step 1: Create a lead first
+	    // Step 1: Create a Contact first
 	    CreateContactClassic_POJO contact = mapper.readValue(
 	            getClass().getClassLoader().getResourceAsStream("api_testData/contactAllFieldPayload.json"),
 	            CreateContactClassic_POJO.class
 	    );
 	    String contactId = postContact(contact);
+	    System.out.println("contactId is :"+contactId);
 
 	    // Step 2: Update some fields
 	  
 	   
-	    contact.setContactName("Univese2");
-	    contact.setOrganizationName("AWS");
-	    contact.setTitle("AI@");
-	    contact.setMobile("9000000121");
+	    contact.setContactName("UniveseGoogle");
+	    contact.setOrganizationName("Google");
+	    contact.setTitle("GoogleTest");
+	    contact.setMobile("9999988888");
 
 	    // Step 3: Send PUT request and capture response
 	    CreateContactClassic_POJO updatedContactPayload = 
@@ -133,10 +135,10 @@ public class Contact_APITest extends api_BaseTest {
 
 	    // Step 4: Assertions to validate updated fields
 	    assert updatedContactPayload.getContactId().equals(contactId): "contact ID mismatch after update";
-	    assert updatedContactPayload.getOrganizationName().equals("AWS") : "Oraganization name not updated correctly";
-	    assert updatedContactPayload.getTitle().equals("AI@"):"Title is not updated correctly";
-	    assert updatedContactPayload.getContactName().equals("Univese2"):"contact name is not updated correctly";
-	    assert updatedContactPayload.getMobile().equals("9000000121"):"mobile is not updated updated correctly";
+	    assert updatedContactPayload.getOrganizationName().equals("Google") : "Oraganization name not updated correctly";
+	    assert updatedContactPayload.getTitle().equals("GoogleTest"):"Title is not updated correctly";
+	    assert updatedContactPayload.getContactName().equals("UniveseGoogle"):"contact name is not updated correctly";
+	    assert updatedContactPayload.getMobile().equals("9999988888"):"mobile is not updated updated correctly";
 	    
 
 	    test.get().pass("contact updated successfully and validated");
@@ -156,9 +158,106 @@ public class Contact_APITest extends api_BaseTest {
 	            .path("contactId"); // capture the created contact ID
 	}
 
+	
+	// Delete Contact
+		// -------------------------------
+		@Test(priority = 1,enabled=false)//Test is working
+		public void deleteLead() throws IOException {
+		    test.set(extent.createTest("Delete Contact Test"));
+
+		    ObjectMapper mapper = new ObjectMapper();
+
+		    // Step 1: Create a Contact first
+		    CreateContactClassic_POJO contact = mapper.readValue(
+		            getClass().getClassLoader().getResourceAsStream("api_testData/contactAllFieldPayload.json"),
+		            CreateContactClassic_POJO.class
+		    );
+		    String contactId = postContact(contact);
+		    System.out.println("Created contact ID: " + contactId);
+
+		    // Step 2: Send DELETE request
+		    given()
+		        .queryParam("contactId", contactId)
+		        .queryParam("campaignId", prop.getProperty("campaignId"))
+		    .when()
+		        .delete("/contact")
+		    .then()
+		        .log().all()
+		        .statusCode(204); // assuming 200 on successful deletion
+
+		
+		    
+		    given()
+		    .queryParam("contactId", contactId)
+		    .queryParam("campaignId", prop.getProperty("campaignId"))
+		.when()
+		    .get("/contact/all")
+		.then()
+		    .statusCode(200);//404
+
+		    test.get().pass("Contact deleted successfully and verified");
+		}
+		
+	
+	//
+	@Test(priority = 1, enabled = true)//tets working
+	public void getAllContactsPaginated() {
+	    test.set(extent.createTest("Get All Contacts"));
+
+	    Response response = 
+	    	given()
+	    	 .queryParam("page", 0)
+	            .queryParam("size", 20)
+	        .when()
+	            .get("/contact/all")
+	        .then()
+	            .log().all()
+	            .statusCode(200) // Assert request success
+	            .extract()
+	            .response();
+	   
+
+	    test.get().pass("Get all contact API executed successfully and validated");
+	}
+
+	@Test(priority = 2, enabled = true)//test working
+	public void getAllContacts() {
+	    test.set(extent.createTest("Get All Contacts - Paginated"));
+
+	    Response response = 
+	    	given()
+	        .when()
+	            .get("/contact/all-contacts")
+	        .then()
+	            .log().all()
+	            .statusCode(200) // Assert request success
+	            .extract()
+	            .response();
+	   
+
+	    test.get().pass("Paginated get all contact API executed successfully and validated");
+	}
 
 	
-	
+	@Test(priority = 3,enabled=true)//test working
+	public void getContactCount() {
+	    test.set(extent.createTest("Get Contact Count"));
+
+	    int contactCount = 
+	    	given()
+	        .when()
+	            .get("/contact/count")
+	        .then()
+	            .log().all()
+	            .statusCode(200) // Ensure request succeeded
+	            .extract()
+	            .as(Integer.class); // Deserialize the response as an integer
+
+	    // Assertion
+	    assert contactCount >= 0 : "Contact count should be 0 or more";
+
+	    test.get().pass("Contact count retrieved successfully: " + contactCount);
+	}
 	
 
 }
