af39acf6cbda1c1f081da252bdc99d6255ec9c5b
Teena Raghav <teenaraghav3017@gmail.com>
2025-08-22 11:50:03 -0500

    commiting user changes (#14)
    Co-authored-by: EmeaTester <emeatest2023@gmail.com>

---
+++ src/main/java/api_tests/R_User.java
@@ -0,0 +1,58 @@
+package api_tests;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class R_User extends api_BaseTest {
+    
+    @Test(priority = 1, enabled = true)
+    public void createUserWithMandatoryFields() throws IOException {
+        test.set(extent.createTest("Verify User Creation with Mandatory Fields"));
+
+        // Headers
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        // Request Body
+        String requestBody = "{\n" +
+                "  \"department\": \"Test4\",\n" +
+                "  \"designation\": \"QA3\",\n" +
+                "  \"dob\": \"07/24/2003\",\n" +
+                "  \"email\": \"sk6@gmail.com\",\n" +
+                "  \"empName\": \"SrK\",\n" +
+                "  \"experience\": 1,\n" +
+                "  \"mobileNo\": \"1934567897\",\n" +
+                "  \"role\": \"IT\",\n" +
+                "  \"username\": \"Ramen\",\n" +
+                "  \"password\": \"123478\"\n" +
+                "}";
+
+        // Send POST request
+        Response response = RestAssured
+                .given()
+                    .auth().preemptive().basic("rmgyantra", "rmgy@9999")
+                    .headers(headers)
+                    .body(requestBody)
+                .when()
+                    .post("/admin/create-user")   
+                .then()
+                    .log().all()
+                    .extract().response();
+
+        // Assertions
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 201, "Expected status code is 201 but found " + statusCode);
+
+        String username = response.jsonPath().getString("username");
+        Assert.assertEquals(username, "Ramen", "Username mismatch!");
+    }
+}
+
+
+
